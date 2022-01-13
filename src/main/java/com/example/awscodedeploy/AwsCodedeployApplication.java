fdcf07ee41ae5ec14d32ec46d821521b43f4bb1e
even <even366@qq.com>
2022-01-13 18:22:08 +0800

    aws

---
+++ src/main/java/com/example/awscodedeploy/AwsCodedeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.awscodedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodedeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsCodedeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awscodedeploy/AwsCodedeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awscodedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodedeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
